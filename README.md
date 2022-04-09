e394a401394bf710c83bbf0da91226eeda1ebd4f
drfudd2001 <elmer.figueroagarcia@gmail.com>
2022-04-09 12:46:26 -0500

    Finished

---
+++ README.md
@@ -0,0 +1,13 @@
+# books-api
+
+## Routes that are defined:
+
+| Method | Path         | Purpose                              |
+| ------ | ------------ | ------------------------------------ |
+| GET    | `/`          | "Hello World"                        |
+| GET    | `/books`     | Books index page, list all books     |
+| POST   | `/books`     | Create a new book                    |
+| GET    | `/books/:id` | Details about a particular book      |
+| PUT    | `/books/:id` | Update a particular book             |
+| DELETE | `/books/:id` | Delete a particular book             |
+| GET    | `*`          | 404 page (not currently implemented) |
--- controllers/books.js
+++ controllers/books.js
@@ -1,95 +1,99 @@
-const express = require('express')
-const books = express.Router()
-const Book = require('../models/book.js')
+const router = require("express").Router();
+const Book = require("../models/book");
 
-// Seed the database (run once!)
-books.get('/seed', (req, res) => {
-  Book.insertMany([{
-    "title": "The Shinobi Initiative",
-    "description": "The reality-bending adventures of a clandestine service agency in the year 2166",
-    "year": 2014,
-    "quantity": 10,
-    "imageURL": "https://imgur.com/LEqsHy5.jpeg"
-  },
-  {
-    "title": "Tess the Wonder Dog",
-    "description": "The tale of a dog who gets super powers",
-    "year": 2007,
-    "quantity": 3,
-    "imageURL": "https://imgur.com/cEJmGKV.jpg"
-  },
-  {
-    "title": "The Annals of Arathrae",
-    "description": "This anthology tells the intertwined narratives of six fairy tales.",
-    "year": 2016,
-    "quantity": 8,
-    "imageURL": "https://imgur.com/VGyUtrr.jpeg"
-  },
-  {
-    "title": "W∀RP",
-    "description": "A time-space anomaly folds matter from different points in earth's history in on itself, sending six unlikely heroes on a race against time as worlds literally collide.",
-    "year": 2010,
-    "quantity": 4,
-    "imageURL": "https://imgur.com/qYLKtPH.jpeg"
-  }])
-  .then(res.status(200).json({
-    message: 'Seed successful'
-  }))
-  .catch(res.status(400).json({
-    message: 'Seed unsuccessful'
-  }))
-})
+router.get("/seed", (req, res) => {
+    Book.insertMany([
+        {
+            title: "The Shinobi Initiative",
+            description: "The reality-bending adventures of a clandestine service agency in the year 2166",
+            year: 2014,
+            quantity: 10,
+            imageURL: "https://imgur.com/LEqsHy5.jpeg",
+        },
+        {
+            title: "Tess the Wonder Dog",
+            description: "The tale of a dog who gets super powers",
+            year: 2007,
+            quantity: 3,
+            imageURL: "https://imgur.com/cEJmGKV.jpg",
+        },
+        {
+            title: "The Annals of Arathrae",
+            description: "This anthology tells the intertwined narratives of six fairy tales.",
+            year: 2016,
+            quantity: 8,
+            imageURL: "https://imgur.com/VGyUtrr.jpeg",
+        },
+        {
+            title: "W∀RP",
+            description: "A time-space anomaly folds matter from different points in earth's history in on itself, sending six unlikely heroes on a race against time as worlds literally collide.",
+            year: 2010,
+            quantity: 4,
+            imageURL: "https://imgur.com/qYLKtPH.jpeg",
+        },
+    ])
+        .then(
+            res.status(200).json({
+                message: "Seed successful",
+            })
+        )
+        .catch(
+            res.status(400).json({
+                message: "Seed unsuccessful",
+            })
+        );
+});
 
-// Individual book
-books.get('/:id', (req, res) => {
-  Book.findById(req.params.id)
-  .then(foundBook => {
-    res.json(foundBook)
-  })
-  .catch(err => {
-    res.json([])
-  })
-})
+router.get("/", (req, res) => {
+    // res.send("Hellooooo books!");
+    Book.find()
+        .then((books) => {
+            res.json(books);
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+});
 
-// Index:
-books.get('/', (req, res) => {
-  Book.find()
-  .then(foundBooks => {
-    res.json(foundBooks)
-  })
-  .catch(err => {
-    res.json("Error")
-  })
-})
+router.post("/", (req, res) => {
+    Book.create(req.body)
+        .then((createdBook) => {
+            res.json(createdBook);
+        })
+        .catch((err) => {
+            res.status(404).json({error: err});
+        });
+});
 
-books.post('/', (req, res) => {
-  Book.create(req.body)
-  .then(createdBook => {
-    res.json(createdBook)
-  })
-  .catch(err => {
-    res.json("Error")
-  })
-})
+router.get("/:id", (req, res) => {
+    Book.findById(req.params.id)
+        .then((foundBook) => {
+            res.json(foundBook);
+        })
+        .catch((err) => {
+            // console.log(err);
+            res.status(404).json({message: "Book not found", error: err});
+        });
+});
 
-books.put('/:id', (req, res) => {
-  Book.findByIdAndUpdate(req.params.id, req.body, { new: true }) 
-    .then(updatedBook => {
-      res.json(updatedBook)
-    })
-    .catch(err => {
-      res.json("Error")
-    })
-  })
+router.put("/:id", (req, res) => {
+    Book.findByIdAndUpdate(req.params.id, req.body, {new: true})
+        .then((updatedBook) => {
+            res.json(updatedBook);
+        })
+        .catch((err) => {
+            res.status(404).json({error: err});
+        });
+});
 
-books.delete('/:id', (req, res) => {
-  Book.findByIdAndDelete(req.params.id) 
-  .then(deletedBook => { 
-    res.json(deletedBook)
-  })
-  .catch(err => {
-    res.json("Error")
-  })
-})
+router.delete("/:id", (req, res) => {
+    Book.findByIdAndDelete(req.params.id)
+        .then((deletedBook) => {
+            res.json({message: "Book deleted successfully"});
+        })
+        .catch((err) => {
+            res.json({error: err});
+        });
+});
 
-module.exports = books
+module.exports = router;
